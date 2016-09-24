38be5867129a89f35ea3548f69d3c61688a975c5
Chris Sevilleja <chris@scotch.io>
2016-09-24 15:45:53 -0700

    07-about-users

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -1,9 +1,62 @@
 import { Component } from '@angular/core';
 
+export class User {
+  id: number;
+  name: string;
+  username: string;
+  avatar: string;
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'Chris',
+    username: 'sevilayha',
+    avatar: 'https://pbs.twimg.com/profile_images/422816823302561793/Hg3LD8K7_400x400.jpeg'
+  },
+  {
+    id: 2,
+    name: 'Nick',
+    username: 'whatnicktweets',
+    avatar: 'https://pbs.twimg.com/profile_images/502500686588690432/wXBzuCBj_400x400.jpeg'
+  },
+  {
+    id: 3,
+    name: 'Holly',
+    username: 'hollylawly',
+    avatar: 'https://pbs.twimg.com/profile_images/721918869821005824/2qT_RY5M_400x400.jpg'
+  }
+]
+
 @Component({
   selector: 'about-page',
+  styles: [`
+    .profile-card   {
+      background: #f3f3f3;
+      border-radius: 4px;
+      padding: 30px;
+      text-align: center;
+    }
+    .profile-card img  {
+      max-width: 50%;
+      margin: 15px auto;
+    }
+  `],
   template: `
-    i am the about page
+    <div class="row" *ngIf="users">
+  
+      <div class="col-sm-4" *ngFor="let user of users">
+        <div class="profile-card">
+          <img [src]="user.avatar" class="img-responsive img-circle">
+
+          <h2>{{ user.name }}</h2>
+          <p><a href="#">{{ user.username }}</a></p>
+        </div>
+      </div>
+
+    </div>
   `
 })
-export class AboutComponent {}
+export class AboutComponent {
+  users: User[] = users;
+}
--- dist/about/about.component.js
+++ dist/about/about.component.js
@@ -9,13 +9,41 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var User = (function () {
+    function User() {
+    }
+    return User;
+}());
+exports.User = User;
+var users = [
+    {
+        id: 1,
+        name: 'Chris',
+        username: 'sevilayha',
+        avatar: 'https://pbs.twimg.com/profile_images/422816823302561793/Hg3LD8K7_400x400.jpeg'
+    },
+    {
+        id: 2,
+        name: 'Nick',
+        username: 'whatnicktweets',
+        avatar: 'https://pbs.twimg.com/profile_images/502500686588690432/wXBzuCBj_400x400.jpeg'
+    },
+    {
+        id: 3,
+        name: 'Holly',
+        username: 'hollylawly',
+        avatar: 'https://pbs.twimg.com/profile_images/721918869821005824/2qT_RY5M_400x400.jpg'
+    }
+];
 var AboutComponent = (function () {
     function AboutComponent() {
+        this.users = users;
     }
     AboutComponent = __decorate([
         core_1.Component({
             selector: 'about-page',
-            template: "\n    i am the about page\n  "
+            styles: ["\n    .profile-card   {\n      background: #f3f3f3;\n      border-radius: 4px;\n      padding: 30px;\n      text-align: center;\n    }\n    .profile-card img  {\n      max-width: 50%;\n      margin: 15px auto;\n    }\n  "],
+            template: "\n    <div class=\"row\" *ngIf=\"users\">\n  \n      <div class=\"col-sm-4\" *ngFor=\"let user of users\">\n        <div class=\"profile-card\">\n          <img [src]=\"user.avatar\" class=\"img-responsive img-circle\">\n\n          <h2>{{ user.name }}</h2>\n          <p><a href=\"#\">{{ user.username }}</a></p>\n        </div>\n      </div>\n\n    </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AboutComponent);
