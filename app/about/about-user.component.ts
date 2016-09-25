6904af2578a3d4c2bab1b116d15387c2145b8308
Chris Sevilleja <chris@scotch.io>
2016-09-24 20:41:54 -0700

    09-get-route-parameter

--- app/about/about-user.component.ts
+++ app/about/about-user.component.ts
@@ -1,8 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+export class User {
+  id: number;
+  name: string;
+  username: string;
+  avatar: string;
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'Chris',
+    username: 'sevilayha',
+    avatar: 'https://pbs.twimg.com/profile_images/422816823302561793/Hg3LD8K7_400x400.jpeg'
+  },
+  {
+    id: 2,
+    name: 'Nick',
+    username: 'whatnicktweets',
+    avatar: 'https://pbs.twimg.com/profile_images/502500686588690432/wXBzuCBj_400x400.jpeg'
+  },
+  {
+    id: 3,
+    name: 'Holly',
+    username: 'hollylawly',
+    avatar: 'https://pbs.twimg.com/profile_images/721918869821005824/2qT_RY5M_400x400.jpg'
+  }
+]
 
 @Component({
+  styles: [`
+    img     {
+      max-width: 300px;
+      margin: 20px auto;
+    }
+  `],
   template: `
-    i am an about user page. 
+    <div class="jumbotron text-center" *ngIf="user">
+      <h1>{{ user.name }} ({{ user.username }})</h1>
+
+      <img [src]="user.avatar" class="img-responsive img-circle">
+    </div>
   `
 })
-export class AboutUserComponent {}
+export class AboutUserComponent implements OnInit {
+  user;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // grab the current username
+    let username = this.route.snapshot.params['username'];
+
+    this.user = users.find(function(user) {
+      return user.username === username;
+    });
+  }
+
+}
--- dist/about/about-user.component.js
+++ dist/about/about-user.component.js
@@ -9,14 +9,50 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var AboutUserComponent = (function () {
-    function AboutUserComponent() {
+var router_1 = require('@angular/router');
+var User = (function () {
+    function User() {
     }
+    return User;
+}());
+exports.User = User;
+var users = [
+    {
+        id: 1,
+        name: 'Chris',
+        username: 'sevilayha',
+        avatar: 'https://pbs.twimg.com/profile_images/422816823302561793/Hg3LD8K7_400x400.jpeg'
+    },
+    {
+        id: 2,
+        name: 'Nick',
+        username: 'whatnicktweets',
+        avatar: 'https://pbs.twimg.com/profile_images/502500686588690432/wXBzuCBj_400x400.jpeg'
+    },
+    {
+        id: 3,
+        name: 'Holly',
+        username: 'hollylawly',
+        avatar: 'https://pbs.twimg.com/profile_images/721918869821005824/2qT_RY5M_400x400.jpg'
+    }
+];
+var AboutUserComponent = (function () {
+    function AboutUserComponent(route) {
+        this.route = route;
+    }
+    AboutUserComponent.prototype.ngOnInit = function () {
+        // grab the current username
+        var username = this.route.snapshot.params['username'];
+        this.user = users.find(function (user) {
+            return user.username === username;
+        });
+    };
     AboutUserComponent = __decorate([
         core_1.Component({
-            template: "\n    i am an about user page. \n  "
+            styles: ["\n    img     {\n      max-width: 300px;\n      margin: 20px auto;\n    }\n  "],
+            template: "\n    <div class=\"jumbotron text-center\" *ngIf=\"user\">\n      <h1>{{ user.name }} ({{ user.username }})</h1>\n\n      <img [src]=\"user.avatar\" class=\"img-responsive img-circle\">\n    </div>\n  "
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [router_1.ActivatedRoute])
     ], AboutUserComponent);
     return AboutUserComponent;
 }());
